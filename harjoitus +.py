76edcce61f51f4dfb557f2d14a6d1544f55b8a45
Maks6666 <118250600+Maks6666@users.noreply.github.com>
2022-11-16 15:23:11 +0200

    Add files via upload

---
+++ harjoitus +.py
@@ -0,0 +1,63 @@
+# # 1
+# x = int(input("x = "))
+# y = int(input("y = "))
+# a = int(input("a = "))
+# def donbass(x):
+#     print (x + y + a)
+# donbass(x)
+
+# 2
+# n = input("Mikä on sinun nimi? ")
+# def azov(n):
+#      print("Terve " + n)
+# azov(n)
+
+# 3
+# s = 1234
+# x = int(input("Mikä on salasana? "))
+# if x == s:
+#     print("salasana on oikein")
+# else:
+#     print("salasana on väärin")
+
+# 4
+# print("Kolmion pinta ala")
+# a = int(input("Mikä on korkeus? = "))
+# h = int(input("Mikä on kanta?  = "))
+# def nsdap(a):
+#     print(a * h)
+# nsdap(a)
+# 
+# print("Suorakulmion pinta ala")
+# b = int(input("Mikä on korkeus? = "))
+# c = int(input("Mikä on pituus?  = "))
+# def kpsu(b):
+#     print(b * c)
+# kpsu(b)
+# 
+# print("Neliön pinta ala")
+# t = int(input("Mikä on korkeus? = "))
+# def nsrpu(t):
+#     print(t * 4)
+# nsrpu(t)
+
+# print("Ympärän pinta ala")
+# r = float(input("Mikä on säde? = "))
+# def dominion(r):
+#     print(r * r * 3,14)
+# dominion(r)
+
+import random
+y = random.randint(10, 20)
+# print(y)
+x = int(input("x = "))
+if x == y:
+    print("olet oikeassa")
+elif x != y:
+    print("et ole oikeassa")
+if x < 10:
+     print("lian pieni")
+elif x > 20:
+     print("lian suuri")
+
+#   print("oikein")
