5e8fc0daebbee574360b3e68f86d3f8ed6b8d658
Maks6666 <118250600+Maks6666@users.noreply.github.com>
2023-08-07 15:13:22 +0300

    Update harjoitus2.py

--- harjoitus2.py
+++ harjoitus2.py
@@ -1,90 +1,100 @@
 # #1
-# y = int(input("y = "))
-# x = int(input("x = "))
-# def wehrmacht(x, y):
+#Summa calculator 
+# y = int(input("y = ")) #User inputs fist number
+# x = int(input("x = ")) #User inputs second number
+# def _sum(x, y): #Function creating
 #     print(x + y)
-# wehrmacht(x, y)
+# _sum(x, y) #Function invitation 
 # 
 # #2
-# y = int(input("y = "))
-# x = int(input("x = "))
-# i = int(input("i = "))
-# def Churchill(x, y, i):
-#     print((x + y + i)/3)
-# Churchill(x,y,i)
+#Avarage number calculator 
+# y = int(input("y = "))  #User inputs fist number
+# x = int(input("x = "))  #User inputs second number
+# i = int(input("i = "))  #User inputs third number
+# def av(x, y, i): #Function creating
+#     print((x + y + i)/3) #Calculation using avarageness formula
+# av(x,y,i) #Function invitation 
 
 #3
-# def ussr(x):
+#Even numbers calculator from a list
+# def even(x): #Function creating
 #     for i in x:
-#         if i % 2 == 0:
+#         if i % 2 == 0: #Programm calculates even numbers by division on 0
 #             print(i)
-# lista = [2, 3, 4, 5, 6, 7]
-# ussr(lista)
+# lista = [2, 3, 4, 5, 6, 7] #List, from which programm choose even numbers
+# even(lista)  #Function invitation 
 # 
 # #4
-# def reich(x):
+##Odd numbers calculator from a list
+# def odd(x):  #Function creating
 #     for i in x:
-#         if i % 2 > 0:
+#         if i % 2 > 0:  #Programm calculates even numbers by division on 0
 #             print(i)
-# lista = [2, 3, 4, 5, 6, 7]
-# reich(lista)
+# lista = [2, 3, 4, 5, 6, 7]  #List, from which programm choose even numbers
+# odd(lista) #Function invitation 
 # 
 # #5
-# y = int(input("y = "))
-# def usa(y):
+#Programm, which increase users number by 1
+# y = int(input("y = ")) #User inputs own number
+# def function(y):  #Function creating
 #       print(y + 1)
-# usa(y)
+# function(y) #Function invitation 
 
 #6
-# y = int(input("y = "))
-# def stalin(y):
-#      print(y + y * -2)
-# stalin(y)
-
-# 7
-def btr(a):
-
-    res1 = a[0]
-    for i in a:
-        if res1 < i:
-            res1 = i
-    return res1
-
-lista1 = (1, 2, 3, 4, 5, 6)
-
-print(btr(lista1))
-
-def bmp(b):
-
-    res2 = b[0]
-    for p in b:
-        if res2 > p:
-            res2 = p
-    return res2
-
-lista2 = (1, 2, 3, 4, 5, 6)
-
-print(bmp(lista2))
+#Programm, which change users number value to oposite
+# y = int(input("y = ")) #User inputs own number
+# def oposite(y):  #Function creating
+#      print(y + y * -2) #Result
+# oposite(y) #Function invitation 
 
 
-def mussolini(x):
-    sum_x = 0
-    for i in x:
-        sum_x = sum_x + i           
+# # 7
+#Programm, with 3 functions to calculate the smallest, the biggest and average value from a list
+# def maximum(a):  #Function creating
 
-    k = sum_x / len(x)
-    return k
+#     res1 = a[0] #Choosing the first value from the list 
+#     for i in a:
+#         if res1 < i: #Comparing it to others
+#             res1 = i
+#     return res1 #Result returning
 
-print("Keskiarvo on", mussolini([1, 2, 3, 4, 5, 6]))
+# lista1 = (1, 2, 3, 4, 5, 6) #The list 
 
-# 8
+# print(maximum(lista1)) #Function invitation and printing the result
 
-def x(n):
-    if n <= 1:
-        return n
-    else:
-        return(x(n-1) + x(n - 2))
+# def minimum(b):  #Function creating
+
+
+#     res2 = b[0] #Choosing the first value from the list 
+#     for p in b:
+#         if res2 > p:  #Comparing it to others
+#             res2 = p
+#     return res2  #Result returning 
+
+# lista2 = (1, 2, 3, 4, 5, 6) #The list 
+
+# print(minimum(lista2))  #Function invitation and printing the result
+
+
+
+# def avarage(x): #Function creating
+#     sum_x = 0 #Sum of numbers in list 
+#     for i in x:
+#         sum_x = sum_x + i   #Adding each element of the list to the sum
+
+#     k = sum_x / len(x) #"K" is for avarage value, programm divides sum of each numbers in list by amount of numbers in list 
+#     return k #Programm returns the average value 
+
+# print("Avarage value is ", avarage([1, 2, 3, 4, 5, 6])) #Programm prints results 
+
+# # 8
+#Programm to calculate fibonacci numbers
+# def x(n):  #Function creating
+#     if n <= 1: #comprasion a variable to 1
+#         return n #Result returning 
+#     else:
+#         return(x(n-1) + x(n - 2)) #Result returning if variable smaller than 1
     
-nterms = 20
-for i in range(nterms):
-    print(x(i))
+# nterms = 20 #Second variable
+# for i in range(nterms): 
+#     print(x(i)) #Result printint
