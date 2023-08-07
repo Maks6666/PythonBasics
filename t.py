e473bbadb55afcaf614c9ecfd345e6e238567aad
Maks6666 <118250600+Maks6666@users.noreply.github.com>
2023-08-07 15:24:25 +0300

    Update idcheck.py
    This program checks does any ID number exist or not

--- t.py
+++ t.py
@@ -1,25 +1,28 @@
-def tarkista_tilinumero(tilinumero):
-    
-    tilinumero = tilinumero.replace(" ", "")
-    if len(tilinumero) != 18:
-        return False
 
-    
-    tilinumero = tilinumero[4:] + tilinumero[:4]
 
+#ID number check programm
+# def tarkista_tilinumero(tilinumero): #Function creating     
     
-    tilinumero = "".join(str(ord(c) - 55) if c.isalpha() else c for c in tilinumero)
+#     tilinumero = tilinumero.replace(" ", "") #Programm deletes all enters
+#     if len(tilinumero) != 18: #If ID number contains less than 18 numbers, programm returns false value
+#         return False
 
     
-    jakojäännös = int(tilinumero) % 97
-    return jakojäännös == 1
+#     tilinumero = tilinumero[4:] + tilinumero[:4] #Programm arranges numbers in correct order 
+
+    
+#     tilinumero = "".join(str(ord(c) - 55) if c.isalpha() else c for c in tilinumero) #Programm turns laters into numbers 
+
+    
+#     jakojäännös = int(tilinumero) % 97
+#     return jakojäännös == 1 #Programm checks is modified ID number devide to 97
 
 
 
-tilinumero = input("Anna tilinumerosi")
-print(tilinumero)
-if tarkista_tilinumero(tilinumero):
-    print("Tilinumerosi on aito.")
-else:
-    print("Tilinumerosi ei ole aito.")
+# tilinumero = input("Anna tilinumerosi") #User input some numbers in random way 
+# print(tilinumero)
+# if tarkista_tilinumero(tilinumero):
+#     print("Tilinumerosi on aito.") #If ID number exists, programm gives such text
+# else:
+#     print("Tilinumerosi ei ole aito.")  #If ID number doesnt exist, programm gives such text
 
