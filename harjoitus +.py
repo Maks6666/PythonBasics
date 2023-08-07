641770ea773a18ca56128dfc681dd77dd676431c
Maks6666 <118250600+Maks6666@users.noreply.github.com>
2023-08-07 13:25:14 +0300

    Update harjoitus +.py
    Some basic Python-exercises

--- harjoitus +.py
+++ harjoitus +.py
@@ -1,63 +1,83 @@
 # # 1
-# x = int(input("x = "))
-# y = int(input("y = "))
-# a = int(input("a = "))
-# def donbass(x):
-#     print (x + y + a)
-# donbass(x)
+#Summa generator
+# x = int(input("x = ")) #User inputs the fist number
+# y = int(input("y = ")) #User inputs the second number
+# a = int(input("a = ")) #User inputs the third number
+# def _summa(x): #Function creating
+#     print (x + y + a) #Programm gives answer 
+#  _summa(x) #Function invitation 
 
 # 2
-# n = input("Mikä on sinun nimi? ")
-# def azov(n):
-#      print("Terve " + n)
-# azov(n)
+#Name checker 
+# n = input("Input your name") #User inputs own name
+# def name(n):   #Function creating
+#      print("Terve " + n)  
+# name(n) #Function invitation 
+
 
 # 3
-# s = 1234
-# x = int(input("Mikä on salasana? "))
-# if x == s:
-#     print("salasana on oikein")
+#Password checker
+# s = 1234 #Right password 
+# x = int(input("Input the password ")) #User inputs the password
+# if x == s: #Programm checks is users answer right 
+#     print("Password is correct")
 # else:
-#     print("salasana on väärin")
+#     print("Password is incorrect")
 
 # 4
-# print("Kolmion pinta ala")
-# a = int(input("Mikä on korkeus? = "))
-# h = int(input("Mikä on kanta?  = "))
-# def nsdap(a):
+#Triangle area calculator
+
+# a = int(input("Input triangle height ")) #Programm asks what triangle height is
+# h = int(input("Input triangle base ")) # #Programm asks what triangle base is
+# def area(a):  #Function creating
 #     print(a * h)
-# nsdap(a)
+# area(a) #Function invitation 
 # 
-# print("Suorakulmion pinta ala")
-# b = int(input("Mikä on korkeus? = "))
-# c = int(input("Mikä on pituus?  = "))
-# def kpsu(b):
+
+
+#5
+#Rectangle area calculator
+
+# b = int(input("What is rectangle height? ")) #Programm asks what rectangle height is
+# c = int(input("What is rectangle width? ")) #Programm asks what rectangle width is
+# def recarea(b): #Function creating
 #     print(b * c)
-# kpsu(b)
-# 
-# print("Neliön pinta ala")
-# t = int(input("Mikä on korkeus? = "))
-# def nsrpu(t):
-#     print(t * 4)
-# nsrpu(t)
+# recarea(b) #Function invitation 
 
-# print("Ympärän pinta ala")
-# r = float(input("Mikä on säde? = "))
-# def dominion(r):
-#     print(r * r * 3,14)
-# dominion(r)
 
-import random
-y = random.randint(10, 20)
-# print(y)
-x = int(input("x = "))
-if x == y:
-    print("olet oikeassa")
-elif x != y:
-    print("et ole oikeassa")
-if x < 10:
-     print("lian pieni")
-elif x > 20:
-     print("lian suuri")
 
-#   print("oikein")
+
+#6
+#Square area calculator
+
+
+# t = int(input("What is sides width? ")) #Programm asks how long square side is 
+# def sider(t): #Function creating
+#     print(t * 4) #Result
+# sider(t) #Function invitation 
+
+
+#7
+#Circle area calculator
+
+# r = float(input("What is circles radius? ")) #Programm asks what circle radius is
+# def cirarea(r): #Function creating 
+#     print(r * r * 3,14) #Area calculation using formula
+# cirarea(r)  #Function invitation 
+
+#8
+#"Guess the number"
+# import random #Import of "random" library
+# y = random.randint(10, 20) #Answer creating in random way
+
+# x = int(input("Your answer is... ")) #Users answer
+# if x == y:
+#    print("You are right!") #If answer is right, programm prints some text 
+# elif x != y:
+#    print("et ole oikeassa") #If answer is incorrect, programm prints some text  
+# if x < 10:
+#    print("lian pieni") #If answer is incorrect and too small, programm prints some text  
+# elif x > 20:
+#    print("lian suuri") #If answer is incorrect and too big, programm prints some text  
+
+
