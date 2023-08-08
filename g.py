619ac9985960aedae35245696032cf4734a04d91
Maks6666 <118250600+Maks6666@users.noreply.github.com>
2023-08-08 10:30:57 +0300

    Update g.py

--- g.py
+++ g.py
@@ -1,43 +1,44 @@
-def HasEnoughtMana(Manapool, cost):
-    #Laskee numeroiden määrä
-    NumeroSymboleita = 0
-    for i in range(len(cost)):
-        if cost[i].isdigit() == True:
+#Mana-game 
+# def HasEnoughtMana(Manapool, cost):
+#     #Program counts amount of numbers 
+#     NumeroSymboleita = 0
+#     for i in range(len(cost)):
+#         if cost[i].isdigit() == True:
             
-            NumeroSymboleita += 1
-        #print( NumeroSymboleita)
+#             NumeroSymboleita += 1
+#         #print( NumeroSymboleita)
             
-    #Määrittele numerollisemn hinnan ja värillisen hinnan
+#     #Define a more numerical price and a colored price 
             
-    Neutraalihinta = 0
-    if Neutraalihinta > 0:
+#     Neutraalihinta = 0
+#     if Neutraalihinta > 0:
     
     
-        Neutraalihinta = cost[0:NumeroSymboleita]
-        Neutraalihinta = int(Neutraalihinta)
-    Värihintä = list(cost[NumeroSymboleita:])
+#         Neutraalihinta = cost[0:NumeroSymboleita]
+#         Neutraalihinta = int(Neutraalihinta)
+#     Värihintä = list(cost[NumeroSymboleita:])
  
     
     
     
-    print(Värihintä)
-    #Onko hintä maksettavissa.
+#     print(Värihintä)
+#     #Program checks does price payable
     
-    for i in range (len(Värihintä)):
-        if Värihintä[i] in Manapool:
-            Manapool = Manapool.replace(Värihintä[i], "",1)
+#     for i in range (len(Värihintä)):
+#         if Värihintä[i] in Manapool:
+#             Manapool = Manapool.replace(Värihintä[i], "",1)
             
-        else:
-            return False
-        #Tarkistaa neutraalin manan
+#         else:
+#             return False
+#         #Program checks neutrals "Mana"s
         
-    if len(Manapool) < Neutraalihinta:
-        return False
-    return True
+#     if len(Manapool) < Neutraalihinta:
+#         return False
+#     return True
         
-        
+    #Program gives a game result    
 
-print (HasEnoughtMana("RGUBWC", "11RG"))
-print (HasEnoughtMana("WWBB", "WWW"))
-print (HasEnoughtMana("RRR", "3"))
-print (HasEnoughtMana("RGG", "RRG"))
+# print (HasEnoughtMana("RGUBWC", "11RG"))
+# print (HasEnoughtMana("WWBB", "WWW"))
+# print (HasEnoughtMana("RRR", "3"))
+# print (HasEnoughtMana("RGG", "RRG"))
